8e884e4e81a4748f95a4bcee82a571f8e6a2ab3c
ShiKaiWi <xykwei@gmail.com>
2018-10-26 20:31:17 +0800

    update the scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 WORKDIR=$GOPATH/src/github.com/ShiKaiWi/mattermost-coderunner-plugin/
 cd $WORKDIR
-GOOS=linux GOARCH=amd64 go build -o coderunner-plugin plugin/plugin.go
-cp plugin/plugin.yaml coderunner-plugin.yaml
+GOOS=linux GOARCH=amd64 go build -o coderunner-plugin server/plugin.go
+cp server/plugin.yaml coderunner-plugin.yaml
 tar czf coderunner-plugin.tar.gz coderunner-plugin coderunner-plugin.yaml
 rm -rf coderunner-plugin.yaml coderunner-plugin
