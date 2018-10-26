ebab0f6b1b45196487e4e31a84b904f8054fcf73
ShiKaiWi <xykwei@gmail.com>
2018-10-26 21:23:05 +0800

    add web plugin demo & update build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,16 @@
 WORKDIR=$GOPATH/src/github.com/ShiKaiWi/mattermost-coderunner-plugin/
 cd $WORKDIR
-GOOS=linux GOARCH=amd64 go build -o coderunner-plugin server/plugin.go
-cp server/plugin.yaml coderunner-plugin.yaml
-tar czf coderunner-plugin.tar.gz coderunner-plugin coderunner-plugin.yaml
-rm -rf coderunner-plugin.yaml coderunner-plugin
+
+echo "building server-coderunner-plugin..."
+GOOS=linux GOARCH=amd64 go build -o plugin server/plugin.go
+cp server/plugin.yaml ./
+tar czf coderunner-plugin.tar.gz plugin plugin.yaml
+rm -rf plugin.yaml plugin
+
+echo "building web-coderunner-plugin..."
+cd web && npm run build && cd -
+WEB_PLUGIN_DIR=com.mattermost.web-coderunner-plugin
+mkdir -p $WEB_PLUGIN_DIR
+cp web/dist/main.js web/plugin.json $WEB_PLUGIN_DIR
+tar czf web-coderunner-plugin.tar.gz $WEB_PLUGIN_DIR
+rm -rf $WEB_PLUGIN_DIR
